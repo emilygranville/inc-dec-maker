53c6760f3b18a91d0c1dd07439ed0f7d194d1516
Emily <32372751+emilygranville@users.noreply.github.com>
2024-12-13 17:16:42 -0500

    clean up

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 
-const form = document.getElementById("form");
 const submit_button = document.getElementById("submit");
 const count = document.getElementById("count");
 const direction = document.getElementById("direction");
