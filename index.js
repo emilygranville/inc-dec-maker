7d93353327420a12e564f932f4444ef683a90699
Emily <32372751+emilygranville@users.noreply.github.com>
2024-12-13 17:27:57 -0500

    allows for other rates of decrease or increase

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 
 const submit_button = document.getElementById("submit");
 const count = document.getElementById("count");
+const rate = document.getElementById("rate");
 const direction = document.getElementById("direction");
 const results = document.getElementById("results");
 
 submit_button.addEventListener("click", function() {
     const target = count.value;
+    const currentRate = rate.value ? rate.value : 6;
 
-    if(!checkSupportedTarget(target)) {
+    if(!checkSupportedTarget(target, currentRate)) {
         tryAgain();
     } else {
-        showInstructions(target, (direction.value == "inc"));
+        showInstructions(target, currentRate, (direction.value == "inc"));
     }
 });
 
 
-function checkSupportedTarget(targetNum) {
-    return targetNum % 6 == 0 ? true : false;
+function checkSupportedTarget(targetNum, rateNum) {
+    return targetNum % rateNum == 0 ? true : false;
 }
 
-function showInstructions(targetNum, isInc) {
+function showInstructions(targetNum, rateNum, isInc) {
     results.innerHTML = "";
 
     let wordsList = [];
     if (isInc) {
-        wordsList = generateIncWords(targetNum);
+        wordsList = generateIncWords(targetNum, rateNum);
     } else {
-        wordsList = generateDecWords(targetNum);
+        wordsList = generateDecWords(targetNum, rateNum);
     }
     
     wordsList.map(sent => {
@@ -36,32 +38,32 @@ function showInstructions(targetNum, isInc) {
     });
 }
 
-function generateIncWords(targetNum) {
+function generateIncWords(targetNum, rateNum) {
     let instructionsList = [];
-    for (let i = 0; i < (targetNum / 6); i++) {
+    for (let i = 0; i < (targetNum / rateNum); i++) {
         if (i == 0) {
-            instructionsList.push("6st into magic circle");
+            instructionsList.push(`${rateNum}st into magic circle`);
         } else if (i == 1) {
-            instructionsList.push("Inc to 12—inc all around");
+            instructionsList.push(`Inc to ${rateNum * 2}—inc all around`);
         } else {
-            instructionsList.push(`Inc to ${(i + 1) * 6}—${i == 2 ? "" : (i - 1)}sc, inc, repeat`)
+            instructionsList.push(`Inc to ${(i + 1) * rateNum}—${i == 2 ? "" : (i - 1)}sc, inc, repeat`)
         }
     }
     return instructionsList;
 }
 
-function generateDecWords(targetNum) {
+function generateDecWords(targetNum, rateNum) {
     let instructionsList = [];
-    for (let i = (targetNum / 6) - 2; i >= 0; i--) {
+    for (let i = (targetNum / rateNum) - 2; i >= 0; i--) {
         if (i == 0) {
-            instructionsList.push("Dec to 6—dec all around");
+            instructionsList.push(`Dec to ${rateNum}—dec all around`);
         } else {
-            instructionsList.push(`Dec to ${(i + 1) * 6}—${i == 1 ? "" : i}sc, dec, repeat`)
+            instructionsList.push(`Dec to ${(i + 1) * rateNum}—${i == 1 ? "" : i}sc, dec, repeat`)
         }
     }
     return instructionsList;
 }
 
 function tryAgain() {
-    window.alert("Please ensure that the target stitch count is a multiple of 6");
+    window.alert("Please ensure that the target stitch count is a multiple of the rate");
 }
